0332009b5c1279e1a29828af1ef8f8b5eb9b4e6f
kongphop <kongphop@tisco.co.th>
2022-10-09 00:29:27 +0700

    init script

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from src.insert_sqlite import insert_data, create_table
 from src.xml_file_handle import read_file
 
 if __name__ == "__main__":
-    data = read_file('resources/data-devclub-1.xml')
-    for idx, elm in enumerate(data):
-        data[idx]["BIRTHDAY"] = datetime.strptime(elm["BIRTHDAY"], "%d-%m-%Y").strftime("%Y-%m-%d")
-        data[idx]["HIRED"] = datetime.strptime(elm["HIRED"], "%d-%m-%Y").strftime("%Y-%m-%d")
-    create_table("master")
-    insert_data("master", data)
+    a = datetime.strptime("2005-02-20", "%Y-%m-%d")
+    b = datetime.now()
+    timedelta(year=3)
+    print(a - b)
+
+# if __name__ == "__main__":
+#     data = read_file('resources/data-devclub-1.xml')
+#     anomaly_idx = []
+#     for idx, elm in enumerate(data):
+#         data[idx]["BIRTHDAY"] = datetime.strptime(elm["BIRTHDAY"], "%d-%m-%Y").strftime("%Y-%m-%d")
+#         data[idx]["HIRED"] = datetime.strptime(elm["HIRED"], "%d-%m-%Y").strftime("%Y-%m-%d")
+#     create_table("master")
+#     insert_data("master", data)
---
+++ src/query_by_nationality.sql
@@ -0,0 +1,8 @@
+WITH DevClub AS (SELECT *, STRFTIME('%Y-%m-%d', HIRED) AS new_date
+                 FROM master
+                 WHERE new_date <= DATE('now', '-3 year')
+                   AND POSITION IN ('Airhostess', 'Pilot', 'Steward')
+                   AND STATUS = 1
+                 ORDER BY new_date DESC)
+SELECT *
+FROM DevClub WHERE NATIONALITY = 'Italy'
