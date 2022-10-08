43c1f005d409ef7623c38e85c4a3205263b8f1e1
kongphop <kongphop@tisco.co.th>
2022-10-08 23:12:37 +0700

    update db

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+from datetime import datetime
+
 from src.insert_sqlite import insert_data, create_table
 from src.xml_file_handle import read_file
 
 if __name__ == "__main__":
     data = read_file('resources/data-devclub-1.xml')
+    for idx, elm in enumerate(data):
+        data[idx]["BIRTHDAY"] = datetime.strptime(elm["BIRTHDAY"], "%d-%m-%Y").strftime("%Y-%m-%d")
+        data[idx]["HIRED"] = datetime.strptime(elm["HIRED"], "%d-%m-%Y").strftime("%Y-%m-%d")
     create_table("master")
     insert_data("master", data)
