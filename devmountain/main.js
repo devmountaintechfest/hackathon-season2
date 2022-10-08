19dc66a66ded7e6f81029f014c32ae1fdb5bf19b
Wutthiphon Main PC <woodychgamer5588@gmail.com>
2022-10-09 00:58:55 +0700

    init seq

--- devmountain/main.js
+++ devmountain/main.js
@@ -1,13 +1,39 @@
 const express = require("express");
 const app = express();
+var Sequelize = require("sequelize");
 
 app.use("/", express.static(__dirname + "/web_interface/dist"));
 
 app.get("/api", (req, res) => {
-    res.json({ changed: count });
+    res.json({ test: "Hello World" });
 });
+
+// Sequelize Init
+var sequelize = new Sequelize(
+    "devclub",
+    "",
+    "",
+    {
+        host: "0.0.0.0",
+        dialect: "sqlite",
+        pool: {
+            max: 5,
+            min: 0,
+            idle: 10000
+        },
+        storage: "./web_interface/dist/database.sqlite"
+    }
+);
+sequelize
+    .authenticate()
+    .then(function (err) {
+        console.log("Connection established.");
+    })
+    .catch(function (err) {
+        console.log("Unable to connect to database: ", err);
+    });
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-    console.log("Wutthiphon API Server is running on port: " + PORT);
+    console.log("Server is running on port: " + PORT);
 });
