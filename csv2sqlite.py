a8bf3ada1c80aa0595b688e4140a478c43469e0b
Natworpong Loyswai <28857628+rsxss@users.noreply.github.com>
2022-10-09 15:37:33 +0700

    Perf use sql instead of os call

--- csv2sqlite.py
+++ csv2sqlite.py
@@ -4,8 +4,7 @@ import collections
 import json
 import sqlite3
 
-from os import remove
-from contextlib import closing
+from contextlib import closing, suppress
 
 CSV_FILE = 'data-devclub-1.csv'
 JSON_FILE = 'data-devclub-1.json'
@@ -30,11 +29,6 @@ JOINED_TABLE_SCHEMA = ',\n'.join((f'{struct} {domain}' for struct, domain in TAB
 
 EMPLOYEE_RECORD = collections.namedtuple('EmployeeRecord', ','.join(TABLE_SCHEMA))
 
-try:
-    remove(DATABASE)
-except FileNotFoundError:
-    pass
-
 
 def _json_dumps(objects):
     output = {}
@@ -50,21 +44,20 @@ def main():
         csv_dict_reader = csv.DictReader(infile)
         payload = (list(map(lambda struct: record[struct], TABLE_SCHEMA)) for record in csv_dict_reader)
 
-        with conn as cur:
-            cur.execute(f'CREATE TABLE {TABLE_NAME}({JOINED_TABLE_SCHEMA})')
-            cur.execute(f'CREATE VIEW `VIEW_EMP_REGION` AS SELECT * FROM {TABLE_NAME} ORDER BY REGION')
-            cur.execute(f'CREATE VIEW `VIEW_EMP_DEPARTMENT` AS SELECT * FROM {TABLE_NAME} ORDER BY DEPT')
-            cur.execute(f'CREATE VIEW `VIEW_EMP_NATIONALITY` AS SELECT * FROM {TABLE_NAME} ORDER BY NATIONALITY')
+        with suppress(sqlite3.IntegrityError), conn as cur:
+            cur.execute(f'CREATE TABLE IF NOT EXISTS {TABLE_NAME}({JOINED_TABLE_SCHEMA})')
+            cur.execute(f'CREATE VIEW IF NOT EXISTS `VIEW_EMP_REGION` AS SELECT * FROM {TABLE_NAME} ORDER BY REGION')
+            cur.execute(f'CREATE VIEW IF NOT EXISTS `VIEW_EMP_DEPARTMENT` AS SELECT * FROM {TABLE_NAME} ORDER BY DEPT')
+            cur.execute(f'CREATE VIEW IF NOT EXISTS `VIEW_EMP_NATIONALITY` AS SELECT * FROM {TABLE_NAME} ORDER BY NATIONALITY')
 
             cur.executemany(
                 f'INSERT INTO {TABLE_NAME} VALUES (?{", ?" * (len(TABLE_SCHEMA) - 1)})',
                 payload)
 
-        with conn as cur:
             result_sets = [
-                cur.execute('SELECT * FROM `VIEW_EMP_REGION` WHERE REGION = ?', ('Europe',)),
-                cur.execute('SELECT * FROM `VIEW_EMP_DEPARTMENT` WHERE DEPT = ?', ('Flight Planning',)),
-                cur.execute('SELECT * FROM `VIEW_EMP_NATIONALITY` WHERE NATIONALITY = ?', ('Ukraine',))
+                cur.execute('SELECT * FROM `VIEW_EMP_REGION` WHERE REGION = ?', (args.region_param,)),
+                cur.execute('SELECT * FROM `VIEW_EMP_DEPARTMENT` WHERE DEPT = ?', (args.dept_param,)),
+                cur.execute('SELECT * FROM `VIEW_EMP_NATIONALITY` WHERE NATIONALITY = ?', (args.nation_param,))
             ]
             outfile.write(_json_dumps(result_sets))
 
@@ -88,6 +81,24 @@ if __name__ == '__main__':
         "--table-name",
         default='DEVCLUB',
         help="Specify the name of a table"
+    )
+
+    parser.add_argument(
+        "--region-param",
+        default='Europe',
+        help="Specify parameter for SQLite View region-based query"
+    )
+
+    parser.add_argument(
+        "--dept-param",
+        default='Flight Planning',
+        help="Specify parameter for SQLite View department-based query"
+    )
+
+    parser.add_argument(
+        "--nation-param",
+        default='Ukraine',
+        help="Specify parameter for SQLite View nationality-based query"
     )
 
     args = parser.parse_args()
