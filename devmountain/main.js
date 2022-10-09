a15a51ffaf41c9a89174d0ce5c2bd1c82e8a7e60
Thanaphol Thangthaweesuk <thanaphol.iqtha@gmail.com>
2022-10-09 12:55:25 +0700

    finish create view api

--- devmountain/main.js
+++ devmountain/main.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const bodyParser = require('body-parser');
 var Sequelize = require("sequelize");
+const { QueryTypes } = require('sequelize');
 
 app.use(bodyParser.json());
 app.use("/", express.static(__dirname + "/web_interface/dist"));
@@ -81,6 +82,36 @@ app.get("/api", (req, res) => {
         res.send(data);
     })
 });
+
+app.get("/api/create/view/dept", (req, res) => {
+    sequelize.query("SELECT DISTINCT DEPT FROM devclub", { type: QueryTypes.SELECT }).then((depts) => {
+        depts.forEach(dept => {
+            sequelize.query("DROP VIEW IF EXISTS :view_name", { type: QueryTypes.SELECT, replacements: { view_name: "devclub_dept_" + dept.DEPT } })
+            sequelize.query("CREATE VIEW :view_name AS SELECT * FROM devclub WHERE DEPT = :dept_data", { replacements: { view_name: "devclub_dept_" + dept.DEPT, dept_data: dept.DEPT } })
+        });
+        res.send(depts);
+    })
+})
+
+app.get("/api/create/view/region", (req, res) => {
+    sequelize.query("SELECT DISTINCT REGION FROM devclub", { type: QueryTypes.SELECT }).then((regions) => {
+        regions.forEach(region => {
+            sequelize.query("DROP VIEW IF EXISTS :view_name", { type: QueryTypes.SELECT, replacements: { view_name: "devclub_region_" + region.REGION } })
+            sequelize.query("CREATE VIEW :view_name AS SELECT * FROM devclub WHERE REGION = :region_data", { replacements: { view_name: "devclub_region_" + region.REGION, region_data: region.REGION } })
+        });
+        res.send(regions);
+    })
+})
+
+app.get("/api/create/view/nationality", (req, res) => {
+    sequelize.query("SELECT DISTINCT NATIONALITY FROM devclub", { type: QueryTypes.SELECT }).then((nationalitys) => {
+        nationalitys.forEach(nationality => {
+            sequelize.query("DROP VIEW IF EXISTS :view_name", { type: QueryTypes.SELECT, replacements: { view_name: "devclub_nationality_" + nationality.NATIONALITY } })
+            sequelize.query("CREATE VIEW :view_name AS SELECT * FROM devclub WHERE NATIONALITY = :nationality_data", { replacements: { view_name: "devclub_nationality_" + nationality.NATIONALITY, nationality_data: nationality.NATIONALITY } })
+        });
+        res.send(nationalitys);
+    })
+})
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
