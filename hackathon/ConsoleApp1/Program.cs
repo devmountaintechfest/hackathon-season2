7d46b4189209535ecaeb3f9563381a1026afdb26
prem_atip <prem.atip.superdev@gmail.com>
2022-10-09 14:33:55 +0700

    save change

--- hackathon/ConsoleApp1/Program.cs
+++ hackathon/ConsoleApp1/Program.cs
@@ -8,8 +8,39 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            string pathFile = "";
-            /* string pathFile = @"D:/DevClubData.xml";*/
+            bool stop = false;
+            string[] cmd =
+            {
+                "stop program"
+            };
+            string menu = "";
+            while (!stop)
+            {
+                for (int idx = 0; idx< cmd.Length;idx++)
+                {
+                    Console.WriteLine((idx+1) + " : "+ cmd[idx]);
+                }
+                Console.WriteLine("please select manu:");
+                menu = Console.ReadLine();
+                for (int idx = 0; idx < cmd.Length; idx++)
+                {
+                    if (menu == (idx + 1).ToString())
+                    {
+                        if (cmd[idx] == "stop program")
+                        {
+                            stop = true;
+                            Console.WriteLine("Good bye");
+                        }
+                        // else if anther menu
+                    }
+                    else
+                    {
+                        Console.WriteLine("don't match please try again\n");
+                    }  
+                }
+            }
+           /* string pathFile = "";
+            *//* string pathFile = @"D:/DevClubData.xml";*//*
             Console.Write("enter url xml file");
             pathFile = Console.ReadLine();
             List<Employees> employee = GlobalFunction.XmlToCsv(pathFile);
@@ -18,7 +49,7 @@ namespace ConsoleApp1
                 Console.WriteLine(emp.ToString());
             }
             //List<Employees> employeeXml = GlobalFunction.csvToXml("../../DefaultDevMountainData.csv");
-            Console.ReadLine();
+            Console.ReadLine();*/
         }
         
     }
