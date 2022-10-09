d4cc6ca193647d0dc6285dc245c8ed4d27412a53
ShiragaP <shiragap@prisma-studio.net>
2022-10-09 21:51:06 +0900

    Add NEW WORLD

--- Assets/XML2CSV/XML2CSVConverterEditor.cs
+++ Assets/XML2CSV/XML2CSVConverterEditor.cs
@@ -63,6 +63,8 @@ public class XML2CSVConverterEditor : Editor
         List<string> acceptedPositions = new List<string>{"Airhostess", "Pilot", "Steward"};
         DateTime acceptedHiredDate = DateTime.Now.AddYears(-3);
 
+        List<string> countries = new List<string>();
+
 		foreach (var record in dataElements)
         {
 			Dictionary<string, string> recordDictionary = new Dictionary<string, string>();
@@ -83,10 +85,21 @@ public class XML2CSVConverterEditor : Editor
 			foreach (string header in columnArray) recordLine.Add(recordDictionary[header]);
 			string[] dataArray = recordLine.ToArray();
 			sw.WriteLine(string.Join(",", dataArray));
+
+			if (!countries.Contains(recordDictionary["NATIONALITY"]))
+				countries.Add("\"" + recordDictionary["NATIONALITY"] + "\"");
 		}
 		sw.Flush();
 		sw.Close();
 
 		AssetDatabase.Refresh();
+
+		string mapTemplate = "{\"groups\":{\"#66c2a4\":{\"label\":\"\",\"paths\":[" + string.Join(",", countries) + "]}},\"title\":\"Employee by Nationality\",\"hidden\":[\"Prince_Edward_Island_CA\",\"New_Brunswick_CA\",\"Ontario_CA\",\"British_Columbia_CA\",\"Alberta_CA\",\"Saskatchewan_CA\",\"Manitoba_CA\",\"Quebec_CA\",\"Yukon_CA\",\"Nunavut_CA\",\"Newfoundland_and_Labrador_CA\",\"Northwest_Territories_CA\",\"Nova_Scotia_CA\",\"Scotland\",\"Wales\",\"England\",\"Northern_Ireland\",\"USA_Wisconsin\",\"USA_Montana\",\"USA_Minnesota\",\"USA_Washington\",\"USA_Idaho\",\"USA_North_Dakota\",\"USA_Michigan\",\"USA_Maine\",\"USA_Ohio\",\"USA_New_Hampshire\",\"USA_New_York\",\"USA_Vermont\",\"USA_Pennsylvania\",\"USA_Arizona\",\"USA_California\",\"USA_New_Mexico\",\"USA_Texas\",\"USA_Alaska\",\"USA_Louisiana\",\"USA_Mississippi\",\"USA_Alabama\",\"USA_Florida\",\"USA_Georgia\",\"USA_South_Carolina\",\"USA_North_Carolina\",\"USA_Virginia\",\"USA_Washington_DC\",\"USA_Maryland\",\"USA_Delaware\",\"USA_New_Jersey\",\"USA_Connecticut\",\"USA_Rhode_Island\",\"USA_Massachusetts\",\"USA_Oregon\",\"USA_Hawaii\",\"USA_Utah\",\"USA_Wyoming\",\"USA_Nevada\",\"USA_Colorado\",\"USA_South_Dakota\",\"USA_Nebraska\",\"USA_Kansas\",\"USA_Oklahoma\",\"USA_Iowa\",\"USA_Missouri\",\"USA_Illinois\",\"USA_Kentucky\",\"USA_Arkansas\",\"USA_Tennessee\",\"USA_West_Virginia\",\"USA_Indiana\"],\"background\":\"#252525\",\"borders\":\"#252525\",\"legendFont\":\"Century Gothic\",\"legendFontColor\":\"#d9d9d9\",\"legendBgColor\":\"#00000000\",\"areBordersShown\":true,\"defaultColor\":\"#737373\",\"labelsColor\":\"#d9d9d9\",\"strokeWidth\":\"medium\",\"areLabelsShown\":true,\"usaStatesShown\":false,\"canadaStatesShown\":false,\"splitUK\":false,\"legendPosition\":\"bottom_left\",\"legendSize\":\"medium\",\"legendStatus\":\"show\",\"scalingPatterns\":true,\"legendRowsSameColor\":true,\"legendColumnCount\":1}";
+		
+		outputPath = AssetDatabase.GetAssetPath(xml2csvConverter);
+		outputPath = outputPath.Substring(0, outputPath.LastIndexOf(xml2csvConverter.name)) + "mapTemplate.json";
+		StreamWriter writer = new StreamWriter(outputPath, true);
+		writer.WriteLine(mapTemplate);
+		writer.Close();
 	}
 }
