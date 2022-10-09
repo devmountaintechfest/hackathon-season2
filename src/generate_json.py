2f0867d0c401d0fd5dfe4ffd8276b9781de2c753
kongphop <kongphop@tisco.co.th>
2022-10-09 14:40:38 +0700

    clean code

--- src/generate_json.py
+++ src/generate_json.py
@@ -4,10 +4,10 @@ import sys
 
 
 def generate_json(path: str, json_file_name: str):
-    with open(path) as f:
-        data = [{k: v for k, v in row.items()} for row in csv.DictReader(f, skipinitialspace=True)]
-        with open(json_file_name, "w") as f:
-            json.dump(data, f)
+    with open(path) as csv_file:
+        data = [{key: val for key, val in row.items()} for row in csv.DictReader(csv_file, skipinitialspace=True)]
+        with open(json_file_name, "w") as json_file:
+            json.dump(data, json_file)
 
 
 if __name__ == "__main__":
