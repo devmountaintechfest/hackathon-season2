b5e6b2909abf791218e4ac40f437c7e7c68e5af3
prem_atip <prem.atip.superdev@gmail.com>
2022-10-09 14:35:36 +0700

    add readline

--- hackathon/ConsoleApp1/Program.cs
+++ hackathon/ConsoleApp1/Program.cs
@@ -39,17 +39,18 @@ namespace ConsoleApp1
                     }  
                 }
             }
-           /* string pathFile = "";
-            *//* string pathFile = @"D:/DevClubData.xml";*//*
-            Console.Write("enter url xml file");
-            pathFile = Console.ReadLine();
-            List<Employees> employee = GlobalFunction.XmlToCsv(pathFile);
-            foreach (var emp in employee)
-            {
-                Console.WriteLine(emp.ToString());
-            }
-            //List<Employees> employeeXml = GlobalFunction.csvToXml("../../DefaultDevMountainData.csv");
-            Console.ReadLine();*/
+            /* string pathFile = "";
+             *//* string pathFile = @"D:/DevClubData.xml";*//*
+             Console.Write("enter url xml file");
+             pathFile = Console.ReadLine();
+             List<Employees> employee = GlobalFunction.XmlToCsv(pathFile);
+             foreach (var emp in employee)
+             {
+                 Console.WriteLine(emp.ToString());
+             }
+             //List<Employees> employeeXml = GlobalFunction.csvToXml("../../DefaultDevMountainData.csv");
+             Console.ReadLine();*/
+            Console.ReadLine();
         }
         
     }
