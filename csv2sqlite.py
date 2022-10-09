dc94477b94b05e267b2f368b935f7c8ec7aaa942
Natworpong Loyswai <28857628+rsxss@users.noreply.github.com>
2022-10-09 18:22:38 +0700

    Add .gitignore and format code

--- csv2sqlite.py
+++ csv2sqlite.py
@@ -4,30 +4,7 @@ import collections
 import json
 import sqlite3
 
-from contextlib import closing, suppress
-
-CSV_FILE = 'data-devclub-1.csv'
-JSON_FILE = 'data-devclub-1.json'
-DATABASE = 'data-devclub-1.db'
-TABLE_NAME = 'DEVCLUB'
-TABLE_SCHEMA = {
-    'EMPID': 'INTEGER PRIMARY KEY',
-    'PASSPORT': 'CHAR(11) NOT NULL UNIQUE',
-    'FIRSTNAME': 'VARCHAR(255)',
-    'LASTNAME': 'VARCHAR(255)',
-    'GENDER': 'INT2',
-    'BIRTHDAY': 'CHAR(10)',
-    'NATIONALITY': 'VARCHAR(56)',
-    'HIRED': 'CHAR(10)',
-    'DEPT': 'VARCHAR(56)',
-    'POSITION': 'VARCHAR(56)',
-    'STATUS': 'TINYINT',
-    'REGION': 'VARCHAR(85)',
-}
-
-JOINED_TABLE_SCHEMA = ',\n'.join((f'{struct} {domain}' for struct, domain in TABLE_SCHEMA.items()))
-
-EMPLOYEE_RECORD = collections.namedtuple('EmployeeRecord', ','.join(TABLE_SCHEMA))
+from contextlib import closing
 
 
 def _json_dumps(objects):
@@ -40,65 +17,91 @@ def _json_dumps(objects):
 
 
 def main():
-    with closing(sqlite3.connect(DATABASE)) as conn, open(CSV_FILE, 'r') as infile, open(JSON_FILE, 'w') as outfile:
+    with closing(sqlite3.connect(DATABASE)) as conn, open(
+        CSV_FILE, "r"
+    ) as infile, open(JSON_FILE, "w") as outfile:
         csv_dict_reader = csv.DictReader(infile)
-        payload = (list(map(lambda struct: record[struct], TABLE_SCHEMA)) for record in csv_dict_reader)
+        payload = (
+            list(map(lambda struct: record[struct], TABLE_SCHEMA))
+            for record in csv_dict_reader
+        )
 
-        with suppress(sqlite3.IntegrityError), conn as cur:
-            cur.execute(f'CREATE TABLE IF NOT EXISTS {TABLE_NAME}({JOINED_TABLE_SCHEMA})')
-            cur.execute(f'CREATE VIEW IF NOT EXISTS `VIEW_EMP_REGION` AS SELECT * FROM {TABLE_NAME} ORDER BY REGION')
-            cur.execute(f'CREATE VIEW IF NOT EXISTS `VIEW_EMP_DEPARTMENT` AS SELECT * FROM {TABLE_NAME} ORDER BY DEPT')
-            cur.execute(f'CREATE VIEW IF NOT EXISTS `VIEW_EMP_NATIONALITY` AS SELECT * FROM {TABLE_NAME} ORDER BY NATIONALITY')
+        with conn as cur:
+            cur.execute(
+                f"CREATE TABLE IF NOT EXISTS {TABLE_NAME}({JOINED_TABLE_SCHEMA})"
+            )
+            cur.execute(
+                f"CREATE VIEW IF NOT EXISTS `VIEW_EMP_REGION` AS SELECT * FROM {TABLE_NAME} ORDER BY REGION"
+            )
+            cur.execute(
+                f"CREATE VIEW IF NOT EXISTS `VIEW_EMP_DEPARTMENT` AS SELECT * FROM {TABLE_NAME} ORDER BY DEPT"
+            )
+            cur.execute(
+                f"CREATE VIEW IF NOT EXISTS `VIEW_EMP_NATIONALITY` AS SELECT * FROM {TABLE_NAME} ORDER BY NATIONALITY"
+            )
 
-            cur.executemany(
-                f'INSERT INTO {TABLE_NAME} VALUES (?{", ?" * (len(TABLE_SCHEMA) - 1)})',
-                payload)
+            try:
+                cur.executemany(
+                    f'INSERT INTO {TABLE_NAME} VALUES (?{", ?" * (len(TABLE_SCHEMA) - 1)})',
+                    payload,
+                )
+            except sqlite3.IntegrityError:
+                pass
 
             result_sets = [
-                cur.execute('SELECT * FROM `VIEW_EMP_REGION` WHERE REGION = ?', (args.region_param,)),
-                cur.execute('SELECT * FROM `VIEW_EMP_DEPARTMENT` WHERE DEPT = ?', (args.dept_param,)),
-                cur.execute('SELECT * FROM `VIEW_EMP_NATIONALITY` WHERE NATIONALITY = ?', (args.nation_param,))
+                cur.execute(
+                    "SELECT * FROM `VIEW_EMP_REGION` WHERE REGION = ?",
+                    (args.region_param,),
+                ),
+                cur.execute(
+                    "SELECT * FROM `VIEW_EMP_DEPARTMENT` WHERE DEPT = ?",
+                    (args.dept_param,),
+                ),
+                cur.execute(
+                    "SELECT * FROM `VIEW_EMP_NATIONALITY` WHERE NATIONALITY = ?",
+                    (args.nation_param,),
+                ),
             ]
             outfile.write(_json_dumps(result_sets))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
     parser.add_argument(
         "--csv",
-        default='data-devclub-1.csv',
+        default="data-devclub-1.csv",
         help="Specify a csv file."
     )
-
+    parser.add_argument(
+        "--json",
+        default="data-devclub-1.json",
+        help="Specify a json outfile."
+    )
     parser.add_argument(
         "--database",
-        default='data-devclub-1.db',
-        help="Specify the name of a database file to write to."
+        default="data-devclub-1.db",
+        help="Specify the name of a database file to write to.",
     )
-
     parser.add_argument(
         "--table-name",
-        default='DEVCLUB',
+        default="DEVCLUB",
         help="Specify the name of a table"
     )
-
     parser.add_argument(
         "--region-param",
-        default='Europe',
-        help="Specify parameter for SQLite View region-based query"
+        default="Europe",
+        help="Specify parameter for SQLite View region-based query",
     )
-
     parser.add_argument(
         "--dept-param",
-        default='Flight Planning',
-        help="Specify parameter for SQLite View department-based query"
+        default="Flight Planning",
+        help="Specify parameter for SQLite View department-based query",
     )
-
     parser.add_argument(
         "--nation-param",
-        default='Ukraine',
-        help="Specify parameter for SQLite View nationality-based query"
+        default="Ukraine",
+        help="Specify parameter for SQLite View nationality-based query",
     )
 
     args = parser.parse_args()
@@ -115,4 +118,29 @@ if __name__ == '__main__':
         print("[-] Please specify the name of a table.")
         exit(1)
 
-main()
+    CSV_FILE = args.csv
+    JSON_FILE = args.json
+    DATABASE = args.database
+    TABLE_NAME = args.table_name
+    TABLE_SCHEMA = {
+        "EMPID": "INTEGER PRIMARY KEY",
+        "PASSPORT": "CHAR(11) NOT NULL UNIQUE",
+        "FIRSTNAME": "VARCHAR(255)",
+        "LASTNAME": "VARCHAR(255)",
+        "GENDER": "INT2",
+        "BIRTHDAY": "CHAR(10)",
+        "NATIONALITY": "VARCHAR(56)",
+        "HIRED": "CHAR(10)",
+        "DEPT": "VARCHAR(56)",
+        "POSITION": "VARCHAR(56)",
+        "STATUS": "TINYINT",
+        "REGION": "VARCHAR(85)",
+    }
+
+    JOINED_TABLE_SCHEMA = ",\n".join(
+        (f"{struct} {domain}" for struct, domain in TABLE_SCHEMA.items())
+    )
+
+    EMPLOYEE_RECORD = collections.namedtuple("EmployeeRecord", ",".join(TABLE_SCHEMA))
+
+    main()
--- xml2csv.py
+++ xml2csv.py
@@ -5,10 +5,21 @@ import xml.etree.ElementTree as ET
 from datetime import datetime
 
 attribute_list = (
-    'EMPID', 'PASSPORT', 'FIRSTNAME', 'LASTNAME', 'GENDER', 'BIRTHDAY', 'NATIONALITY', 'HIRED', 'DEPT', 'POSITION',
-    'STATUS', 'REGION')
+    "EMPID",
+    "PASSPORT",
+    "FIRSTNAME",
+    "LASTNAME",
+    "GENDER",
+    "BIRTHDAY",
+    "NATIONALITY",
+    "HIRED",
+    "DEPT",
+    "POSITION",
+    "STATUS",
+    "REGION",
+)
 
-current_datetime = datetime.now()  # Acceptable edge case
+current_datetime = datetime.now()
 
 
 def ensure_years_experiences(date_str, years=3):
@@ -24,13 +35,14 @@ def xml2csv_converter_helper(records, convert_default):
     existed_EMPID_set = set()
     existed_PASSPORT = set()
     for record in records:
-        if convert_default and \
-                (record[10].text != '1' or \
-                 record[9].text not in ('Airhostess', 'Pilot', 'Steward') or \
-                 record[0].text == record[1].text or \
-                 record[0].text in existed_EMPID_set or \
-                 record[1].text in existed_PASSPORT or \
-                 not ensure_years_experiences(record[7].text, years=3)):
+        if convert_default and (
+            record[10].text != "1"
+            or record[9].text not in ("Airhostess", "Pilot", "Steward")
+            or record[0].text == record[1].text
+            or record[0].text in existed_EMPID_set
+            or record[1].text in existed_PASSPORT
+            or not ensure_years_experiences(record[7].text, years=3)
+        ):
             continue
         existed_EMPID_set.add(record[0].text)
         existed_PASSPORT.add(record[1].text)
@@ -41,15 +53,20 @@ def xml2csv_converter(xml_file, csv_file, convert_default):
     tree = ET.parse(xml_file)
     records = tree.getroot()
     csv_by_nationality = dict()
-    with open(csv_file, 'w', newline='') as output_file:
-        csv_writer = csv.writer(output_file, delimiter=',')
+    with open(csv_file, "w", newline="") as output_file:
+        csv_writer = csv.writer(output_file, delimiter=",")
         csv_writer.writerow(attribute_list)
         for record in xml2csv_converter_helper(records, convert_default):
             nationality = record[6]
             if nationality not in csv_by_nationality:
-                csv_by_nationality_file = f'{csv_file[:-4]}-{nationality}.csv' if csv_file[-4:] == '.csv' else csv_file
-                csv_by_nationality[nationality] = csv.writer(open(csv_by_nationality_file, 'w', newline=''),
-                                                             delimiter=',')
+                csv_by_nationality_file = (
+                    f"{csv_file[:-4]}-{nationality}.csv"
+                    if csv_file[-4:] == ".csv"
+                    else csv_file
+                )
+                csv_by_nationality[nationality] = csv.writer(
+                    open(csv_by_nationality_file, "w", newline=""), delimiter=","
+                )
                 csv_by_nationality[nationality].writerow(attribute_list)
             csv_writer.writerow(record)
             csv_by_nationality[nationality].writerow(record)
@@ -60,30 +77,30 @@ def main():
         xml_file = args.xml
         csv_file = args.csv
         convert_default = args.convert_default
-        xml2csv_converter(xml_file=xml_file, csv_file=csv_file, convert_default=convert_default)
+        xml2csv_converter(
+            xml_file=xml_file, csv_file=csv_file, convert_default=convert_default
+        )
     except Exception as e:
         print(e)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
     parser.add_argument(
         "--xml",
-        default='data-devclub-1.xml',
-        help="Specify a xml file."
+        default="data-devclub-1.xml",
+        help="Specify the name of a xml file to read from."
     )
-
     parser.add_argument(
         "--csv",
-        default='data-devclub-1.csv',
-        help="Specify the name of a csv file to write to."
+        default="data-devclub-1.csv",
+        help="Specify the name of a csv file to write to.",
     )
-
     parser.add_argument(
         "--convert-default",
-        action='store_false',
-        help="Only convert actives 3 years experiences+ Airhostess, Pilot, Steward to csv file without duplicate ID and passport number"
+        action="store_false",
+        help="Only convert actives 3 years experiences+ Airhostess, Pilot, Steward to csv file without duplicate ID and passport number.",
     )
 
     args = parser.parse_args()
